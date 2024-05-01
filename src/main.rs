7cfd914203a2414adb17c45c82754c3b9e9997ba
Thomas Laflamme <bc991967@ens.uqam.ca>
2024-04-30 21:51:29 -0400

    Add program arguments/options parsing
    - Add clap dependencie
    - Add Args struct
    - Parse arguments and options

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(author = "Thomas Laflamme", version = "0.1", about = "Utility to compare your program's output to the expected output")]
+struct Args {
+    /// The program to test.
+    prog: String,
+
+    /// The program's arguments.
+    prog_args: Vec<String>,
+
+    #[clap(short = 'e', long = "expected")]
+    /// The expected output file.
+    expected: String,
+
+    #[clap(long = "line-order", default_value_t = true)]
+    /// Should every line be in the exact same order as the expected output. True by default.
+    line_order: bool,
+
+    #[clap(long = "space-format", default_value_t = true)]
+    /// Should a whitespace difference count. True by default.
+    space_format: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    dbg!(args);
 }
